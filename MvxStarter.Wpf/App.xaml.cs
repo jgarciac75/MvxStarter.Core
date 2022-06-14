215e17b0843ca5736ebd195f0405d3ec49355648
Jorge Garcia Casalett <jgarciac75@gmail.com>
2022-06-14 07:06:31 -0500

    Agregar archivos de proyecto.

---
+++ MvxStarter.Wpf/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using MvvmCross.Core;
+using MvvmCross.Platforms.Wpf.Core;
+using MvvmCross.Platforms.Wpf.Views;
+
+
+
+
+namespace MvxStarter.Wpf
+{
+  /// <summary>
+  /// Interaction logic for App.xaml
+  /// </summary>
+  public partial class App : MvxApplication
+  {
+    protected override void RegisterSetup()
+    {
+      //this.RegisterSetupType<MvxWpfSetup<Core.App>>();//base.RegisterSetup();
+      this.RegisterSetupType<WpfSetup>();
+    }
+  }
+}
---
+++ MvxStarter.Wpf/WpfSetup.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Logging;
+using MvvmCross.Platforms.Wpf.Core;
+using Serilog;
+using Serilog.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvxStarter.Wpf
+{
+  public class WpfSetup : MvxWpfSetup<Core.App>
+  {
+    protected override ILoggerFactory CreateLogFactory()
+    {
+
+      // serilog configuration
+      Log.Logger = new LoggerConfiguration()
+          .MinimumLevel.Debug()
+          .WriteTo.Trace()
+          .CreateLogger();
+
+      return new SerilogLoggerFactory();
+
+    }
+
+    protected override ILoggerProvider CreateLogProvider()
+    {
+
+      return new SerilogLoggerProvider();
+    }
+  }
+}
